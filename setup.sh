2b8d99a3bf9dcfecdb1bddd55ee448a0565da491
Jeppe Allerslev <eppeallerslev@gmail.com>
2022-10-05 17:46:07 +0200

    Updated dotfiles

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 #zshrc
 ln -s /home/jeppe/.dotfiles/.zshrc ~/.zshrc
 
-#init.vim
-mkdir /home/jeppe/.config/nvim
-ln -s /home/jeppe/.dotfiles/init.vim ~/.config/nvim/init.vim
-
 #ranger
 rm -r /home/jeppe/.config/ranger
 ln -s /home/jeppe/.dotfiles/ranger ~/.config/ranger
